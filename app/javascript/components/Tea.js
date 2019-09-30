dec45cfe623697d545ec805c3f0b441c1df99025
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2019-09-30 12:46:28 +0900

    Starting like function

--- app/javascript/components/Tea.js
+++ app/javascript/components/Tea.js
@@ -1,20 +1,34 @@
-import React from "react"
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import React from "react";
+import styled from "styled-components";
 import SimpleBottomNavigation from "./SimpleBottomNavigation";
 
-const Tea = ({tea}) => {
-    return (
-      <React.Fragment>
-        <p>Name: {tea.name}</p>
-        <p>Description: {tea.description}</p>
-        <p>Origin: {tea.origin}</p>
+const StyledView = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  color: red;
+`;
+
+const StyledP = styled.p`
+  font-sze: 2em;
+`;
+
+const Tea = ({ tea }) => {
+  return (
+    <React.Fragment>
+      <StyledView>
+        <StyledP>Name: {tea.name}</StyledP>
+        <StyledP>Description: {tea.description}</StyledP>
+        <StyledP>Origin: {tea.origin}</StyledP>
         <SimpleBottomNavigation />
-      </React.Fragment>
-    );
-}
+      </StyledView>
+    </React.Fragment>
+  );
+};
 
 Tea.propTypes = {
   name: PropTypes.string,
   description: PropTypes.string
 };
-export default Tea
+export default Tea;
